03e9cd84ee58870491ff31f3d0c405a9931bb29f
David Gómez Escobar <114017497+D4V1DESP@users.noreply.github.com>
2023-12-08 12:45:23 +0100

    Commit

---
+++ src/app/constants.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+
+
+@NgModule()
+export class ConstantsModule {
+    //public SERVER_URL: string = "https://muevetic-be.onrender.com"
+    public SERVER_URL: string = "http://localhost:8080"
+}
